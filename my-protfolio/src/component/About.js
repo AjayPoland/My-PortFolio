6babb4baf6b2d71ba716df28230662eb4a46edc3
AjayPoland <ajaymanger21@gmail.com>
2023-01-28 19:22:05 +0100

    did some styling on welcome and About component

--- my-protfolio/src/component/About.js
+++ my-protfolio/src/component/About.js
@@ -1,42 +1,57 @@
 import React from "react";
 
 //styling bootstrap css
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 //external css
-import'../css/About.css'
+import "../css/About.css";
 
 function About() {
   return (
     <div className="about-home p-4 rounded-2">
       <h1 className=" about-heading">About</h1>
       <div className=" about-body m-4">
-      <p>
-        I am from India. Currently I am living in Poland since last few years.
-        After working here, I realize that I am in the wrong path. So,decidede
-        to focused to built the carrier in the field of which i belong too. In
-        the year 2015 I complete my study in computer science , but
-        unfertunately I didn't dived into the IT sector. Nowdays I felt myself
-        guilty so, decided to carry forward my IT skills and keep update myself
-        with the new technquie, skill of programming.
-      </p>
-      <p>
-        {" "}
-        Since last 1 year I am studing and preparing for the Job interview and
-        made a habite of learning programming everyday atleast for more then
-        2hours and sometime the whole day. I enjoy programming and making
-        something creative everyday. Finding bugs and fixing it, not repeating
-        the same code multiple times is my key strength. I am the good learner
-        and love to work in the team.
-      </p>
-      <p>
-        {" "}
-        The resources, i am using for learning programming are the open source
-        and physical books of my institute(NIIT) India .I belive as a fresher
-        that I can help the people, organisation working in IT sector.</p>
-        <strong>
-        Some of
-        my recent projects are listed below, do check it out.{" "}
-        </strong>
+        <p>
+          My birth country is India, I grew up there. I did my schooling in
+          India.{" "}
+          <span className="fw-bold">
+            The languages in which i am fluent are (English, Hindi, Nepali,
+            Bengali).
+          </span>{" "}
+          Currently I am living in Poland since last few years. After working
+          here, I realize that I am in the wrong path. So, decideded to focused
+          to built the carrier in the field of which i belong too.{" "}
+          <span className="fw-bold">
+            In the year 2015 I completed my study in computer science{" "}
+          </span>
+          , but unfertunately I didn't dived into the IT sector. Now i am
+          looking for the opertunity to show my skills and keeping myself update
+          with the new technquie, skill of programming.
+        </p>
+        <p>
+          {" "}
+          Since last 1 year I am studing and preparing for the Job interview and
+          made a habite of learning programming everyday atleast for more then
+          2hours and sometime the whole day.
+          <span className="fw-bold">
+            {" "}
+            I enjoy programming and making something creative everyday. Finding
+            bugs and fixing it, not repeating the same code multiple times is my
+            key strength.
+          </span>{" "}
+          I am the good learner and love to work in the team.
+        </p>
+        <p>
+          {" "}
+          The resources, i am using for learning programming are the{" "}
+          <span className="fw-bold">
+            open source and physical books of my institute(NIIT) India{" "}
+          </span>
+          .I belive as a fresher that I can help the people, organisation
+          working in IT sector.
+        </p>
+        <span className="fw-bold">
+          Some of my recent projects are listed below, do check it out.{" "}
+        </span>
       </div>
     </div>
   );
--- my-protfolio/src/component/Welcome.js
+++ my-protfolio/src/component/Welcome.js
@@ -7,22 +7,22 @@ import '../css/welcome.css';
 function Welcome() {
   return (
     <>
-      <div className='mx-5'>
+      <div className='welcome mx-5'>
         <header>
           <h1>Hi!</h1>
         </header>
         <main>
-          <p>I am here to present myself as a software developer. I
-            Prefer to work mostly in the front-end designinng although have a
-            knowledge of back-end programming.
+          <p>I am here to present myself as a <span className="fw-bold">software developer</span> . I
+            Prefer to work mostly in the <span className="fw-bold">front-end designinng</span> although have a
+            knowledge of <span className="fw-bold">back-end programming.</span>
           </p>
           <p>
             Nowadays, the IT world is changing drastically, by the empact of new
-            teachnology, to fullfill the demands of modern world requirement.
+            teachnology, to fullfill the demands of <span className="fw-bold">modern world requirement.</span>
           </p>
           <p>
-            I also want to contribute my skills in this sector and be the part
-            of those teams who all are adding the huge value to the IT industry!
+            I also want to contribute <span className="fw-bold">my skills in this sector</span> and be the part
+            of those teams who all are adding <span className="fw-bold">the huge value to the IT industry!</span>
           </p>
           ThankYou.
         </main>
